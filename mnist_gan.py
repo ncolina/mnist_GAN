78c4c04264c9b7a63ede49c615c5782e2aa0c213
Nicholas Colina <nicholascolina1@gmail.com>
2017-08-07 22:00:49 +0800

    added preview of geneterator output as the model trains

--- mnist_gan.py
+++ mnist_gan.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
-
-
+import matplotlib
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+matplotlib.use('tkagg')
+import matplotlib.pyplot as plt
 
 
 def generator_input(size):
@@ -56,13 +56,14 @@ def optimizer(loss, var_list, algo='adam'):
     return optimizer
 
 
-def generator(input, hdim=100):
+def generator(input, hdim=512):
     h0 = dense_layer(input, hdim, activation='relu', scope='g0')
-    h1 = dense_layer(h0, 784, scope='g1')
-    return h1
+    h1 = dense_layer(h0, hdim, activation='relu', scope='g1')
+    h2 = dense_layer(h1, 784, activation='sigmoid', scope='g2')
+    return h2
 
 
-def discriminator(input, h_dim=100):
+def discriminator(input, h_dim=512):
     h0 = dense_layer(input, h_dim * 2, scope='d0', activation='relu')
     h1 = dense_layer(h0, h_dim * 2, scope='d1', activation='relu')
     h2 = dense_layer(h1, h_dim * 2, scope='d2', activation='relu')
@@ -89,10 +90,10 @@ class GAN(object):
         def sigmoid_cross_entropy_with_logits(x, y):
             try:
                 return tf.nn.sigmoid_cross_entropy_with_logits(logits=x,
-                                                                labels=y)
+                                                               labels=y)
             except:
                 return tf.nn.sigmoid_cross_entropy_with_logits(logits=x,
-                                                                targets=y)
+                                                               targets=y)
 
         # Next we define the loss functions of the D and G
         self.d_loss_real = tf.reduce_mean(sigmoid_cross_entropy_with_logits(
@@ -115,27 +116,47 @@ class GAN(object):
         self.d_opt = optimizer(self.d_loss, self.d_param, algo='adam')
         self.g_opt = optimizer(self.g_loss, self.g_param, algo='adam')
 
-    def train(self, data, n_steps=100, k_steps=10, minibatch_size=128):
+    def train(self, data, n_iter=1000, k_iter=2, minibatch_size=128,
+              preview=False):
         print('Starting Training')
         with tf.Session() as sess:
-                sess.run(tf.global_variables_initializer())
-                for i in range(n_steps):
-                    for j in range(k_steps):
-                        d_loss, d_result = sess.run(
-                                    [self.d_loss, self.d_opt],
-                                    {self.z: data.train.images,
-                                        self.x: generator_input(
-                                            len(data.train.images))})
-                    g_loss, g_result = sess.run(
-                                    [self.g_loss, self.g_opt],
-                                    {self.z: data.train.images,
-                                        self.x: generator_input(
-                                            len(data.train.images))})
-                    if i % 1 == 0:
-                        print("Iteration {}:\tD Loss={:.6f}\tG Loss={:.6f}".format(i, d_loss, g_loss))
+            sess.run(tf.global_variables_initializer())
+            saver = tf.train.Saver()
+            for i in range(n_iter):
+                for j in range(k_iter):
+                    batch = data.train.next_batch(minibatch_size)
+                    d_loss, d_result = sess.run(
+                                [self.d_loss, self.d_opt],
+                                {self.z: batch[0],
+                                    self.x: generator_input(
+                                        len(data.train.images))})
+                batch = data.train.next_batch(minibatch_size)
+                g_loss, g_result = sess.run(
+                                [self.g_loss, self.g_opt],
+                                {self.z: batch[0],
+                                    self.x: generator_input(
+                                        len(data.train.images))})
+                if i % 1 == 0:
+                    print("Iteration {}:\tD Loss={:.6f}\tG Loss={:.6f}"
+                          .format(i, d_loss, g_loss))
+                    if preview:
+                        output = sess.run(self.G,
+                                          {self.x:
+                                           generator_input(size=1)})
+                        output = np.reshape(output, (28, 28))
+                        plt.ion()
+                        plt.imshow(output)
+                        plt.pause(0.001)
+                        plt.axis('off')
+                        plt.pause(0.001)
+                        plt.title('Iteration {}'.format(i))
+                        plt.pause(0.001)
+            saver.save(sess, 'mnist-gan')
+        if preview:
+            plt.show()
 
 
 if __name__ == '__main__':
-    print("Starting")
     model = GAN()
-    model.train(mnist, k_steps=2)
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    model.train(mnist, n_iter=1000, k_iter=2, preview=True, minibatch_size=256)
