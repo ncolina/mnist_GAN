8950d842140a626d302e552590843cce7ca15a9c
Nicholas Colina <nicholascolina1@gmail.com>
2017-08-16 13:43:42 -0400

    fix

--- resume_training.py
+++ resume_training.py
@@ -7,7 +7,7 @@ def restore_session(path):
     return sess
 
 if __name__ == '__main__':
-    model = GAN()
+    model = mnist_gan.GAN()
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
     hist = model.train(mnist, n_iter=20000, k_iter=1,
                        preview=True, minibatch_size=32, resume=True)
